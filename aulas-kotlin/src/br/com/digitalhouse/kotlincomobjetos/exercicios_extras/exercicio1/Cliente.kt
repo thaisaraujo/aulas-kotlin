590e5ad8e0aa60906a5906ef19876bd181905dce
thaisaraujo <thais.araujo.hp@hotmil.com>
2020-09-08 20:56:07 -0300

    acrescentando exe1

---
+++ aulas-kotlin/src/br/com/digitalhouse/kotlincomobjetos/exercicios_extras/exercicio1/Cliente.kt
@@ -0,0 +1,4 @@
+package br.com.digitalhouse.kotlincomobjetos.exercicios_extras.exercicio1
+
+class Cliente (val nome: String, val sobrenome: String){
+}
---
+++ aulas-kotlin/src/br/com/digitalhouse/kotlincomobjetos/exercicios_extras/exercicio1/Conta.kt
@@ -0,0 +1,17 @@
+package br.com.digitalhouse.kotlincomobjetos.exercicios_extras.exercicio1
+
+class Conta (val numeroDaConta: Int, var saldo: Float, val titular: Cliente) {
+
+    fun depositar(valor: Float) {
+        saldo+=valor
+        println("Novo saldo de ${saldo}")
+    }
+
+    fun saque(valor: Float){
+        if(saldo-valor < 0){
+            println("Saldo insuficiente para realizar saque")
+        }else{
+            println("Saque realizado com sucesso. Saldo: ${saldo}")
+        }
+    }
+}
